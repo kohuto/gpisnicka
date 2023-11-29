f1b03298cff747cf72d56b9b3123343e3abc77ed
Your Name <kohu.ondr@email.cz>
2023-11-29 10:17:59 +0100

    updated

--- programovani_algoritmizace/selection_sort.py
+++ programovani_algoritmizace/selection_sort.py
@@ -2,9 +2,9 @@ def bubble_sort_with_error(arr):
     n = len(arr)
     for i in range(n):
         swapped = False
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
                 swapped = True
 
         # Pokud nebyly žádné dva prvky vyměněny vnitřní smyčkou, přeruš cyklus
